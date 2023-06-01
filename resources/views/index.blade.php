6f6f4f693242ff1576b34854d83002fc37768e57
Haidan08 <126222934+Haidan08@users.noreply.github.com>
2023-06-01 15:24:24 +0700

    Add files via upload

---
+++ resources/views/index.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Landing Page</title>
+    <meta name="description" content="Free bootstrap template Atlas">
+    <link rel="icon" href="img/logoA.jpg" sizes="32x32" type="image/png">
+    <!-- custom.css -->
+    <link rel="stylesheet" href="css/custom.css">
+    <!-- bootstrap.min.css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- font-awesome -->
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+
+    <!-- AOS -->
+    <link rel="stylesheet" href="css/aos.css">
+</head>
+
+<body>
+    <!-- banner -->
+    <div class="jumbotron jumbotron-fluid" id="banner" style="background-image: url(img/banner-bk.jpg);">
+        <div class="container text-center text-md-left">
+            <header>
+                <div class="row justify-content-between">
+                    <div class="col-2">
+                        <img src="img/logoA.jpg" alt="logo">
+                    </div>
+                    <div class="col-6 align-self-center text-right">
+                        <a href="/dashboard" class="text-black lead">Dashboard</a>
+                    </div>
+                </div>
+            </header>
+            <h1 data-aos="fade" data-aos-easing="linear" data-aos-duration="1000" data-aos-once="true"
+                class="display-3 text-gold font-weight-bold my-5">
+                Ahsan Store<br>
+                Toko Elektronik
+
+            </h1>
+            <p data-aos="fade" data-aos-easing="linear" data-aos-duration="1000" data-aos-once="true"
+                class="lead text-gold my-4">
+                Welcome To Ahsan Store <br>
+                Semua yang kamu butuhkan ada disini.
+            </p>
+            <a href="#" data-aos="fade" data-aos-easing="linear" data-aos-duration="1000" data-aos-once="true"
+                class="btn my-4 font-weight-bold atlas-cta cta-green">Mulai Sekarang</a>
+        </div>
+    </div>
+    <!-- three-blcok -->
+    <div class="container my-5 py-2">
+        <h2 class="text-center font-weight-bold my-5">Benefit</h2>
+        <div class="row">
+            <div data-aos="fade-up" data-aos-delay="0" data-aos-duration="1000" data-aos-once="true"
+                class="col-md-4 text-center">
+                <img src="..." alt="Anti-spam" class="mx-auto">
+                <h4>Efektifitas</h4>
+                <p>Bisa Mulai Kapan Saja!.</p>
+            </div>
+            <div data-aos="fade-up" data-aos-delay="400" data-aos-duration="1000" data-aos-once="true"
+                class="col-md-4 text-center">
+                <img src="..." alt="Smart Scan" class="mx-auto">
+                <h4>Akurat</h4>
+                <p>DIperbarui Setiap Waktu!</p>
+            </div>
+        </div>
+    </div>
+    <!-- feature (skew background) -->
+    <div class="jumbotron jumbotron-fluid feature" id="feature-first">
+        <div class="container my-5">
+            <div class="row justify-content-between text-center text-md-left">
+                <div data-aos="fade-right" data-aos-duration="1000" data-aos-once="true" class="col-md-6">
+                    <h2 class="font-weight-bold">Barang 100% Original</h2>
+                    <p class="my-4">Barang terjaga dengan aman dan dipastikan tidak ada barang palsu!...</p>
+                    <a href="#" class="btn my-4 font-weight-bold atlas-cta cta-blue">Pelajari Lebih Lanjut</a>
+                </div>
+                <div data-aos="fade-left" data-aos-duration="1000" data-aos-once="true"
+                    class="col-md-6 align-self-center">
+                    <img src="..." alt="Take a look inside" class="mx-auto d-block" style="width:300px">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- feature (green background) -->
+    <div class="jumbotron jumbotron-fluid feature" id="feature-last">
+        <div class="container">
+            <div class="row justify-content-between text-center text-md-left">
+                <div data-aos="fade-left" data-aos-duration="1000" data-aos-once="true" class="col-md-6 flex-md-last">
+                    <h2 class="font-weight-bold">Harga Terjangku</h2>
+                    <p class="my-4">
+                        Barang dijual dengan harga yang terjangkau dan cocok untuk semua kalangan umur!
+                        <br>
+                    </p>
+                    <a href="#" class="btn my-4 font-weight-bold atlas-cta cta-blue">Pelajari Lebih Lanjut</a>
+                </div>
+                <div data-aos="fade-right" data-aos-duration="1000" data-aos-once="true"
+                    class="col-md-6 align-self-center flex-md-first">
+                    <img src="..." alt="Safe and reliable" class="mx-auto d-block">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- copyright -->
+    <div class="jumbotron jumbotron-fluid" id="copyright">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-md-6 text-white align-self-center text-center text-md-left my-2">
+                    Copyright ©haidan2023.
+                </div>
+                <div class="col-md-6 align-self-center text-center text-md-right my-2" id="social-media">
+                    <a href="#" class="d-inline-block text-center ml-2">
+                        <i class="fa fa-facebook" aria-hidden="true"></i>
+                    </a>
+                    <a href="#" class="d-inline-block text-center ml-2">
+                        <i class="fa fa-instagram" aria-hidden="true"></i>
+                    </a>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- AOS -->
+    <script src="js/aos.js"></script>
+    <script>
+        AOS.init({});
+
+    </script>
+</body>
+
+</html>
